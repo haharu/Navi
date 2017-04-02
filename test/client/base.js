229e47d544049143b3d368d8ee987aa7bcfac942
haharuLin <haharu.ame@gmail.com>
2017-04-02 18:50:19 +0800

    base test for history api

--- test/client/base.js
+++ test/client/base.js
@@ -4,6 +4,8 @@ const assert = require('assert');
 const should = require('should');
 const Navi = require('../..');
 
+const testUrl = 'https://www.kkday.com/zh-tw/home/index2.php?hey=ho#foo_bar';
+
 describe('Scope', function() {
     it('should have window global object', function() {
         global.should.be.an.Object;
@@ -12,12 +14,36 @@ describe('Scope', function() {
     })
 })
 
-describe('Parallels', function () {
-    it('should only one thread exist',function () {
+describe('Parallels', function() {
+    it('should only one thread exist', function() {
+        Navi.should.deepEqual(require('../..'));
+        Navi.root.should.deepEqual(window);
+    })
+})
+
+describe('#LinkTo', function() {
+    describe('#hash', function() {
+        it('should be end with #test', function() {
+            let originUrl = window.location.href;
+            Navi.linkTo('#test');
+            Navi.current.href.should.equal(`${originUrl}#test`);
+            Navi.replaceState(Navi.current.pathname);
+            Navi.current.href.should.equal(originUrl);
+        })
+    })
+
+})
+
+describe('#State', function() {
+    it('should pushState to history without page refresh', function() {
         let originUrl = window.location.href;
-        Navi.pushState('#test');
-        Navi.currentUrl().should.equal(`${originUrl}#test`);
-        Navi.should.deepEqual(require('../..'));;
-        Navi.replaceState(originUrl);
+        Navi.pushState('test');
+        Navi.current.href.should.equal(`${Navi.current.origin}/test`);
+        Navi.pushState('/test');
+        Navi.current.href.should.equal(`${Navi.current.origin}/test`);
+        Navi.pushState('/');
+        Navi.current.href.should.equal(`${Navi.current.origin}/`);
+        Navi.pushState(originUrl);
+        Navi.current.href.should.equal(originUrl);
     })
 })
